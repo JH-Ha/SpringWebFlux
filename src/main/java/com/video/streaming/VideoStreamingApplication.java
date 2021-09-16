180bb807580c615b1ba47952084bdb642d7e2cfa
Jaehyeong Ha <cubeprince@gmail.com>
2021-09-17 03:58:59 +0900

    update jar

--- src/main/java/com/video/streaming/VideoStreamingApplication.java
+++ src/main/java/com/video/streaming/VideoStreamingApplication.java
@@ -21,7 +21,7 @@ public class VideoStreamingApplication {
     //public static StreamingService streamingService;
 
     public static void main(String[] args) {
-       
+        System.out.println("started");
         SpringApplication.run(VideoStreamingApplication.class, args);
     }
 
